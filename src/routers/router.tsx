9480b4a0ae311464d1912abe48b9d1721279ebe5
Denis Herrera Espiritu <jsherr.6@gmail.com>
2025-07-03 22:05:43 -0500

    feat(router): include private routes in main router

--- src/routers/router.tsx
+++ src/routers/router.tsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import { publicRoutes } from "./publicRoutes";
 import { ErrorPage } from "../index";
+import { privateRoutes } from "./privateRoutes";
 
 export const router = createBrowserRouter([
   ...publicRoutes,
+  ...privateRoutes,
   {
     path: "*",
     element: <ErrorPage />,
