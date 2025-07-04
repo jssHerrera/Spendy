c45675d8db501d948d21b257d97c1140f1463da4
Denis Herrera Espiritu <jsherr.6@gmail.com>
2025-07-03 21:56:51 -0500

    feat(auth): add PrivateRoute to protect routes for authenticated users only

---
+++ src/hooks/ProtectedRoute.tsx
@@ -0,0 +1,25 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuthStore } from "../index";
+
+type ProtectedRouteProps = {
+  redirectTo: string;
+  children?: React.ReactNode;
+};
+
+export const ProtectedRoute = ({
+  redirectTo,
+  children,
+}: ProtectedRouteProps) => {
+  const { user, isLoading, isInitialized } = useAuthStore();
+
+  // Mostrar loading mientras se inicializa la auth o mientras carga
+  if (!isInitialized || isLoading) {
+    return <div>Cargando sesión...</div>;
+  }
+
+  if (!user) {
+    return <Navigate to={redirectTo} replace />;
+  }
+
+  return children ? children : <Outlet />;
+};
